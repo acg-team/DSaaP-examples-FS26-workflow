bd34867edf373e78d0e5eb4b44872c2e0d1e3537
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2026-04-18 17:36:52 +0200

    Fix test formatting with black and flake8

--- test/test_read_sequences.py
+++ test/test_read_sequences.py
@@ -2,20 +2,24 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from gc_content.gc_content import read_sequences_from_file
 
+
 def test_read_sequences_file_not_found():
     # Function returns an empty list when the file is not found
     result = read_sequences_from_file("non_existent_file.fasta")
     assert result == []
+
 
 def test_read_sequences_non_fasta():
     # Function returns an empty list when the file is not a FASTA file
     result = read_sequences_from_file("test/data/non_fasta.phy")
     assert result == []
 
+
 def test_read_sequences_empty_file():
     # Function returns an empty list when the file is empty
     result = read_sequences_from_file("test/data/DNA_empty.fasta")
     assert result == []
+
 
 def test_read_sequences_valid():
     # Function returns a list of SeqRecord objects when the file is valid
@@ -30,12 +34,13 @@ def test_read_sequences_valid():
     assert result[3].id == "D"
     assert str(result[3].seq) == "GTCTA"
 
+
 def test_read_sequences_mocked(mocker):
     # Mock file open
-    mock_open = mocker.patch('builtins.open', mocker.mock_open())
+    mock_open = mocker.patch("builtins.open", mocker.mock_open())
 
     # Mock SeqIO.parse to return an iterator of SeqRecords
-    mock_parse = mocker.patch('Bio.SeqIO.parse')
+    mock_parse = mocker.patch("Bio.SeqIO.parse")
     mock_record1 = SeqRecord(Seq("ATGC"), id="seq1")
     mock_record2 = SeqRecord(Seq("CGTA"), id="seq2")
     mock_parse.return_value = iter([mock_record1, mock_record2])
@@ -53,9 +58,10 @@ def test_read_sequences_mocked(mocker):
         mock_open.return_value.__enter__.return_value, "fasta-pearson"
     )
 
+
 def test_read_sequences_generic_exception(mocker):
     # Mock file open
-    mock_open = mocker.patch('builtins.open')
+    mock_open = mocker.patch("builtins.open")
 
     # Configure the mock to raise a generic Exception
     mock_open.side_effect = Exception("Generic error")
@@ -63,4 +69,3 @@ def test_read_sequences_generic_exception(mocker):
     result = read_sequences_from_file("dummy.fasta")
 
     assert result == []
-
